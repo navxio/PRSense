2fa080189461e01124b755daa8c4858041497cac
Navdeep Saini <navdeep@mailbox.org>
2026-01-16 10:05:27 +0700

    chore(domain): remove ReviewRule abstraction

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,6 +1,4 @@
 export * from "./signal/ReviewSignal.js";
-export * from "./review/ReviewRule.js";
-export * from "./review/RuleRegistry.js";
 
 export * from "./diff/Diff.js";
 export * from "./context/ReviewContext.js";
--- packages/domain/src/review/RuleRegistry.ts
+++
@@ -1,8 +0,0 @@
-/* Rule Vocabulary */
-import { ReviewRule } from "./ReviewRule.js";
-
-export type RuleId = string;
-
-export type RuleRegistry = {
-  [id: RuleId]: ReviewRule;
-};
