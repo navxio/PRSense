0659e78d8cdc1cd295a8678fa61d56ab15a2e214
Navdeep Saini <navdeep@mailbox.org>
2026-01-20 19:17:27 +0700

    setup default db url

--- packages/config/src/env/schema.ts
+++ packages/config/src/env/schema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 export const EnvConfigSchema = z.object({
-  PRSENSE_DATABASE_URL: z.url(),
+  PRSENSE_DATABASE_URL: z
+    .url()
+    .default(
+      "postgresql://prsense:prsense@localhost:10000/prsense_dev?sslmode=disable",
+    ),
 
   PRSENSE_GITHUB_TOKEN: z.string().optional(),
   PRSENSE_OPENAI_API_KEY: z.string().optional(),
