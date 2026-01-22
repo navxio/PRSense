255015572d96934b8fae54f955524dfbec649824
Navdeep Saini <navdeep@mailbox.org>
2026-01-22 19:57:41 +0700

    move engine elements to core/, remove engine package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,3 +5,7 @@ export * from "./context/ReviewContext.js";
 export * from "./input/ReviewInput.js";
 export * from "./ui/task.js";
 export * from "./ui/tasks.js";
+export * from "./ui/runTask.js";
+export * from "./indexing/index.js";
+export * from "./engine/ReviewEngine.js";
+export * from "./engine/types.js";
--- packages/engine/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./engine/ReviewEngine.js";
-export * from "./ReviewEngine.js";
-export * from "./indexing/index.js";
-export * from "./ui/runTask.js";
