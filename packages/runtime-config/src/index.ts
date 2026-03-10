173f90e9962aa11551acadb7fa5124c655afaf0a
Navdeep Saini <navdeep@mailbox.org>
2026-03-10 18:58:05 +0700

    chore(main): completely remove unneeded prsense/runtime-config package
    also removed from deps of packages that depended on it

--- packages/runtime-config/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./ResolvedConfig.js";
-export * from "./validateResolvedConfig.js";
-export * from "./CredentialContext.js";
-export * from "./runtimeMode.js";
-export * from "./buildCredentialContext.js";
-export * from "./resolveConfig.js";
-export * from "./validateCredentialContext.js";
