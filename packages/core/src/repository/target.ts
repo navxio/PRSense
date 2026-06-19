1ab2a00694a549423ef924b33de75de3e4314927
Navdeep Saini <navdeep@mailbox.org>
2026-06-19 13:19:31 +0530

    expand ClassifiedTarget type to encompass both indexing/review targets

--- packages/core/src/repository/target.ts
+++ packages/core/src/repository/target.ts
@@ -2,23 +2,47 @@
 export type ClassifiedTarget =
   | {
       provider: "github";
+      kind: "pr";
       root: string;
       owner: string;
       repo: string;
       pr: string;
     }
   | {
+      provider: "github";
+      kind: "repo";
+      root: string;
+      owner: string;
+      repo: string;
+    }
+  | {
       provider: "gitlab";
+      kind: "mr";
       root: string;
       group: string;
       project: string;
       mr: string;
     }
   | {
+      provider: "gitlab";
+      kind: "repo";
+      root: string;
+      group: string;
+      project: string;
+    }
+  | {
       provider: "codeberg";
+      kind: "pr";
       root: string;
       owner: string;
       repo: string;
       pr: string;
+    }
+  | {
+      provider: "codeberg";
+      kind: "repo";
+      root: string;
+      owner: string;
+      repo: string;
     }
   | { provider: "filesystem"; root: string };
